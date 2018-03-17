fdcb1442b130b33e4c018eeb93d9615d338914a1
Sylhare <sylhareng@gmail.com>
2018-03-17 11:07:03 -0500

    Add Draw Rectangle

---
+++ src/Examples/drawRectangle.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+void drawRectangle(int l, int h)
+{
+
+    for(int line(0); line < h; line++){
+
+        for(int column(0); column < l; column++){
+            std::cout << "*";
+        }
+
+        std::cout << '\n';
+    }
+}
+
+int main()
+{
+    int width, height;
+
+    std::cout << "This programme will draw a rectangle: \n\n"
+                 "Rectangle's width : ";
+
+    std::cin >> width;
+
+    std::cout << "Rectangle's height : ";
+
+    std::cin >> height;
+
+    std::cout << "\n";
+
+    drawRectangle(width, height);
+
+    return 0;
+}
+
